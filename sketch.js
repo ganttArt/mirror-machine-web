3424292a4004ebca5015a6e6b34188c579586db1
Chris Gantt <gantt.art@gmail.com>
2022-02-06 16:50:20 -1000

    Import mirror image function and call it in the draw function

--- sketch.js
+++ sketch.js
@@ -15,8 +15,26 @@ function setup() {
 function draw() {
   background(0);
   image(img, 0, 0);
+  mirrorImage(.5);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+
+//percentageWidth is between 0.05 to 1 for all functions
+function mirrorImage(percentageWidth) { 
+  background(0);    
+  push();
+  copy(
+    img, 0, 0, int(img.width*percentageWidth), img.height, 
+    0, 0, int(img.width*(percentageWidth)), img.height
+  ); //left side
+  scale(-1, 1);
+  translate(int(-img.width*(2*percentageWidth)), 0);
+  copy(
+    img, 0, 0, int(img.width*percentageWidth), img.height, 
+    1, 0, int(img.width*percentageWidth), img.height); //right side
+  pop();
 }
